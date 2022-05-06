eb93fe1a165436ca3f41d912b85b31a9332ca19e
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-06 21:15:33 +0600

    restock product quantity funtionality added

--- index.js
+++ index.js
@@ -62,8 +62,20 @@ async function run (){
             };
             const result = await ProductCollection.updateOne(filter, updateDoc, option);
             res.send(result)
+        })
 
-
+        app.put('/restock/:id', async (req, res)=>{
+            const id = req.params.id;
+            const restockQuantity = req.body;
+            const filter = {_id: ObjectId(id)};
+            const option = { upsert : true };
+            const updateDoc = {
+                $set : {
+                    quantity: restockQuantity.quantity
+                }
+            };
+            const result = await ProductCollection.updateOne(filter, updateDoc, option);
+            res.send(result)
         })
 
 
