690e91b50e73be4befdb12577430fac760e54764
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-06 23:43:05 +0600

    functionality added for delete api

--- index.js
+++ index.js
@@ -86,7 +86,13 @@ async function run (){
             res.send(result)
         })
 
-
+        //DELETE API
+        app.delete("/product/:id", async (req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await ProductCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
 
